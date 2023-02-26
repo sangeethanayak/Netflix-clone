e7ba26d32beb7653ebbc7c6bbcbd0be3a4dbd200
sangeetha nayak <sangeethanayak333@gmail.com>
2023-02-26 22:36:42 +0530

    initial commit

---
+++ src/BgImage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import background from "../assets/login.jpg";
+
+export default function BgImage() {
+  return (
+    <Container>
+      <img src={background} alt="background" />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  img {
+    height: 100vh;
+    width: 100vw;
+  }
+`;
