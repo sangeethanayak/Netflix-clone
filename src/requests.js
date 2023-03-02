963851093c76c82dfd62e17b32f3f7524cdba413
Sangeetha Nayak <sangeethanayak333@gmail.com>
2023-03-02 22:11:12 +0530

    initial commit

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY='Your_api_key'
+const API_KEY='Your_tmdb_api_key'
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
